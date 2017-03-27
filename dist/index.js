d1f6aa932db9d9ac0290c0f017bcf03ad80a766f
Swizz <gerodel.quentin@gmail.com>
2017-03-27 16:36:04 +0200

    Bump version number and add features notes

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,10 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 var h = _interopDefault(require('snabbdom/h'));
 var extend = _interopDefault(require('extend'));
 
+var svgTags = [
+  'svg', 'circle', 'ellipse', 'line', 'polygon', 'polyline', 'rect', 'g', 'path', 'text'
+];
+
 var sanitizeProps = function (props) {
 
   props = props === null ? {} : props;
@@ -43,8 +47,41 @@ var sanitizeProps = function (props) {
 
 var sanitizeChilds = function (children) {
 
-  return children.length === 1 && typeof children[0] === 'string' ?
-    children[0] : children
+  if (children.length === 1 && typeof children[0] === 'string') {
+
+    return children[0]
+
+  }
+  if (children.reduce(function (acc, curr) { return acc || Array.isArray(curr); }, false)) {
+
+    return children
+      .reduce(function (acc, curr) { return Array.isArray(curr) ? acc.concat( curr) : acc.concat( [curr]); }, [])
+
+  }
+
+  return children
+
+};
+
+var considerSVG = function (props, type) {
+
+  if (svgTags.indexOf(type) > -1) {
+
+    var attrs = Object.assign({}, props.props, props.props.className ? { class: props.props.className } : undefined);
+
+    var p = Object.assign({}, props, { attrs: attrs });
+
+    if (p.attrs.className) {
+
+      delete p.attrs.className;
+
+    }
+
+    delete p.props;
+    return p
+
+  }
+  return props
 
 };
 
@@ -55,7 +92,7 @@ var createElement = function (type, props) {
 
   return (typeof type === 'function') ?
     type(props, children) :
-    h(type, sanitizeProps(props), sanitizeChilds(children))
+    h(type, considerSVG(sanitizeProps(props), type), sanitizeChilds(children))
 
 };
 
