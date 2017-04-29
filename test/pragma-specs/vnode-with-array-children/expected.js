f10added0dbd37266f7e22bdead2710a56deb0a4
Swizz <gerodel.quentin@gmail.com>
2017-04-29 18:33:46 +0200

    Test and build mistakes

--- test/pragma-specs/vnode-with-array-children/expected.js
+++ test/pragma-specs/vnode-with-array-children/expected.js
@@ -3,7 +3,7 @@ import h from 'snabbdom/h'
 
 export default () => {
   return h('div', {}, [
-    h('span', 'Hello', {}, []),
-    h('span', 'World', {}, [])
+    h('span', {}, 'Hello'),
+    h('span', {}, 'World')
   ])
 }
