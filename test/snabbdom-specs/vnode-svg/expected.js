c2d85fb6856d8dedd180c7faca308593b001e0a7
Swizz <gerodel.quentin@gmail.com>
2017-07-08 12:38:31 +0200

    Now attrs is present by default on svg vnode

--- test/snabbdom-specs/vnode-svg/expected.js
+++ test/snabbdom-specs/vnode-svg/expected.js
@@ -1,7 +1,7 @@
 
 export default (h) => {
-  return h('svg', {}, [
-    h('g', {}, [
+  return h('svg', { attrs: {} }, [
+    h('g', { attrs: {} }, [
       h('circle', {
         attrs: { cx: 43.5, cy: 23, r: 5 }
       }, [])
