af35a944969caadd25a30246a222c842ea67909c
Luiz Américo <luizamericop@gmail.com>
2018-05-15 17:23:31 -0300

    Inline check for vnode props

--- src/is.js
+++ src/is.js
@@ -15,15 +15,9 @@ export const object = (v) => typeof v === 'object' && !!v
 
 export const fun = (v) => typeof v === 'function'
 
-const vnodeProps = ['sel', 'data', 'children', 'text', 'elm', 'key']
-
-export const vnode = (v) => object(v) &&
-  vnodeProps.every(
-    (k) => k in v
-  )
+export const vnode = (v) => object(v) && 'sel' in v && 'data' in v && 'children' in v && 'text' in v
 
 const svgPropsMap = {svg: 1, circle: 1, ellipse: 1, line: 1, polygon: 1,
 polyline: 1, rect: 1, g: 1, path: 1, text: 1}  
-
 
 export const svg = (v) => v.sel in svgPropsMap
