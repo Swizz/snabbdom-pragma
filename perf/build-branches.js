876f5a18aae0bbf126b96558ce1420c80c52155e
Luiz Américo <luizamericop@gmail.com>
2018-06-17 14:19:28 -0300

    Fix lint warnings

--- perf/build-branches.js
+++ perf/build-branches.js
@@ -1,13 +1,12 @@
 /* eslint unicorn/no-process-exit: 0 */
 
 const execSync = require('child_process').execSync
-const fs = require('fs')
 const simpleGit = require('simple-git')()
 
 const baseBranch = process.argv[2] || 'develop'
 
 function buildSnabbdom(suffix) {
-  execSync(`npm run build -- --file perf/snabbdom-${suffix}.js`, { stdio: [0, 1, 2] })  
+  execSync(`npm run build -- --file perf/snabbdom-${suffix}.js`, { stdio: [0, 1, 2] })
 }
 
 simpleGit.status((err, status) => {
